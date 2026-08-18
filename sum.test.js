89e7e5cfe5ca7dffc482f80d9d0d82a7f1de7a02
Mratunjay Patle <mratunjaypatle05@gmail.com>
2026-08-18 12:11:47 +0530

    change opr function

--- sum.test.js
+++ sum.test.js
@@ -1,4 +1,5 @@
 import sum from "./sum.js";
+import opr from "./operation.js"
 
 describe("test for multiple cases", () => {
 
@@ -9,7 +10,11 @@ describe("test for multiple cases", () => {
 
     test("add -1 + -10", () => {
         expect(sum(-1, -10)).toBe(-11); 
-
+    })
+    
+    test("multiply 2 * 2 " , () =>
+    {
+        expect(opr(10,2)).toBe(20);
     })
 
 })
